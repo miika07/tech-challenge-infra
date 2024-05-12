166020fc4ea560063510d3980ad17b17ee9bd9fa
Camila Couto <camilagcouto@hotmail.com>
2024-05-12 04:56:32 -0300

    teste

--- vpc.tf
+++ vpc.tf
@@ -7,12 +7,10 @@ module "vpc" {
   public_subnets     = local.public_subnets
   enable_nat_gateway = true
   azs                = local.azs_vpc
-  tags = {
-    Environment = "VPC Tech Challenge - 1"
-  }
 
   tags = {
     "kubernetes.io/cluster/${local.name_cluster}" = "shared",
+    Environment = "VPC Tech Challenge - 1"
   }
 
   public_subnet_tags = {
