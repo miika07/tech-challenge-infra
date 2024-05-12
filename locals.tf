071c060d4395c2c2b96728883abcdf1c81392376
Camila Couto <camilagcouto@hotmail.com>
2024-05-12 00:12:37 -0300

    fix name

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   region          = "us-east-1"
-  name_cluster    = "tech-challenge-cluster"
+  name_cluster    = "tech-challenge-cluster-1"
   name_vpc        = "tech-challenge-vpc"
   cidr            = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
