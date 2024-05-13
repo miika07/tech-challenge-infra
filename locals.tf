e02079ce93ba5f65caf993387c497488903d9b93
Camila Couto <camilagcouto@hotmail.com>
2024-05-13 19:26:09 -0300

    trocar nome vpc

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   region          = "us-east-1"
   name_cluster    = "tech-challenge-eks"
-  name_vpc        = "tech-challenge-vpc-eks"
+  name_vpc        = "tech-challenge-vpc"
   name_sg         = "tecg-challenge_sg"
   cidr            = "10.0.0.0/16"
   private_subnets = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
